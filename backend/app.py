b1ebf358be62884c3d36ee0a296c4b3011401889
PAKALAVAN <darwin1415dar@gmail.com>
2026-04-07 22:28:20 +0530

    add netlify CORS origin

--- backend/app.py
+++ backend/app.py
@@ -109,6 +109,7 @@ app.add_middleware(
         "http://localhost:5173",
         "http://127.0.0.1:3000",
         "http://localhost:3000",
+        "https://bug-triaging.netlify.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
